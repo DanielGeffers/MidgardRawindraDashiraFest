fe938a161c858a1bb103e90bb83f6dbe7bcae0a1
Daniel Geffers <39227292+DanielGeffers@users.noreply.github.com>
2021-02-04 17:35:55 +0100

    croped and removed
    croped weapons to remove error created upon entering labels and flair

--- stuff.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="stuff" tilewidth="32" tileheight="32" tilecount="432" columns="16">
- <image source="weapons_etc.png" width="512" height="867"/>
-</tileset>
